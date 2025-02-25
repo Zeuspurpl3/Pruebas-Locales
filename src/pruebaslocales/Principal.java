11ddd5298af8542a20553c63b5edd4fb34ac8bf9
Michael Hurtado <mhurtadohualca@gmail.com>
2025-02-25 19:26:49 +0100

    segundo commit

--- src/pruebaslocales/Principal.java
+++ src/pruebaslocales/Principal.java
@@ -11,5 +11,6 @@ package pruebaslocales;
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Estamos haciendo una prueba, aprendiendo Git");
+        ComoQuieras.pintarMensaje("OtroMensaje");
     }
 }
