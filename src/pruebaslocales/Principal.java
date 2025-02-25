ab91eadb93dbad0963ae2a8f749d95cfb2cbc6e0
Michael Hurtado <mhurtadohualca@gmail.com>
2025-02-25 19:14:00 +0100

    commit inicial

---
+++ src/pruebaslocales/Principal.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pruebaslocales;
+
+/**
+ *
+ * @author PC
+ */
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Estamos haciendo una prueba, aprendiendo Git");
+    }
+}
