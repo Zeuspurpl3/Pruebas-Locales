9f6df91489d50347da207c18d0e8fdc6d657c683
Michael Hurtado <mhurtadohualca@gmail.com>
2025-02-25 19:28:09 +0100

    Otro texto

---
+++ src/pruebaslocales/ComoQuieras.java
@@ -0,0 +1,17 @@
+package pruebaslocales;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author PC
+ */
+public class ComoQuieras {
+    public static void pintarMensaje(String cadena){
+        System.out.println(cadena);
+    }
+    
+}
